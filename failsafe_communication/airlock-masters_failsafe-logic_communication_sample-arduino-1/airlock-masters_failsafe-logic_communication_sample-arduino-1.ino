6bbc43b899a83c106834d3a2156f1b04b900abce
OmarAliQazi <omaraliqazi@outlook.com>
2017-09-30 12:17:48 -0700

    Sample Code for master-slave arduino updated

--- failsafe_communication/airlock-masters_failsafe-logic_communication_sample-arduino-1/airlock-masters_failsafe-logic_communication_sample-arduino-1.ino
+++ failsafe_communication/airlock-masters_failsafe-logic_communication_sample-arduino-1/airlock-masters_failsafe-logic_communication_sample-arduino-1.ino
@@ -1,9 +1,9 @@
-// Wire Master Reader
+// Wire Master Writer
 // by Nicholas Zambetti <http://www.zambetti.com>
 
 // Demonstrates use of the Wire library
-// Reads data from an I2C/TWI slave device
-// Refer to the "Wire Slave Sender" example for use with this
+// Writes data to an I2C/TWI slave device
+// Refer to the "Wire Slave Receiver" example for use with this
 
 // Created 29 March 2006
 
@@ -13,17 +13,18 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  Wire.begin(); // join i2c bus (address optional for master)
 }
+
+byte x = 0;
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+  Wire.beginTransmission(8); // transmit to device #8
+  Wire.write("x is ");        // sends five bytes
+  Wire.write(x);              // sends one byte
+  Wire.endTransmission();    // stop transmitting
 
-  while (Wire.available()) { // slave may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
-  }
-
+  x++;
   delay(500);
 }
+
--- failsafe_communication/airlock-masters_failsafe-logic_communication_sample-arduino-2/airlock-masters_failsafe-logic_communication_sample-arduino-2.ino
+++ failsafe_communication/airlock-masters_failsafe-logic_communication_sample-arduino-2/airlock-masters_failsafe-logic_communication_sample-arduino-2.ino
@@ -1,9 +1,9 @@
-// Wire Slave Sender
+// Wire Slave Receiver
 // by Nicholas Zambetti <http://www.zambetti.com>
 
 // Demonstrates use of the Wire library
-// Sends data as an I2C/TWI slave device
-// Refer to the "Wire Master Reader" example for use with this
+// Receives data as an I2C/TWI slave device
+// Refer to the "Wire Master Writer" example for use with this
 
 // Created 29 March 2006
 
@@ -14,16 +14,21 @@
 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
 }
 
 void loop() {
   delay(100);
 }
 
-// function that executes whenever data is requested by master
+// function that executes whenever data is received from master
 // this function is registered as an event, see setup()
-void requestEvent() {
-  Wire.write("hello "); // respond with message of 6 bytes
-  // as expected by master
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
 }
