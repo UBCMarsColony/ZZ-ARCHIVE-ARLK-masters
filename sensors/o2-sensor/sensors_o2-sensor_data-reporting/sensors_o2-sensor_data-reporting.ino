2ed6b89980727ca613839c7215ff491b11e614e4
Thomas <trichmond@vigiltechnologies.com>
2017-10-21 14:34:05 -0700

    Fixed some bugs, created a more clear constant for ADC port to reduce confusion

--- sensors/o2-sensor/sensors_o2-sensor_data-reporting/sensors_o2-sensor_data-reporting.ino
+++ sensors/o2-sensor/sensors_o2-sensor_data-reporting/sensors_o2-sensor_data-reporting.ino
@@ -2,27 +2,33 @@
  * Written: October 2017
  */
 
-#define REFERENCE_VOLTAGE 3.3 // voltage of ADC reference
+#define ADC_PORT A0
+
+#define REFERENCE_VOLTAGE_V 5 // voltage of ADC reference
+#define ERROR_CODE -555
 
 //Increase values below to decrease print frequency.
 #define DATA_PRINT_COUNTER_MOD 10
 
 double getO2Concentration(int sensorIn){
+  //Read O2 sensor data
   long sum = 0;
   for(int i = 0; i<32; i++){
     sum += analogRead(sensorIn);
   }
-
   sum >>= 5;
 
-  float voltage = sum * (REFERENCE_VOLTAGE / 1023.0);
+  //Convert O2 sensor data to voltage.
+  float voltage = sum * (REFERENCE_VOLTAGE_V / 1023.0);
 
-  float concentration = voltage * 0.21 / 2.0;
+  if( voltage == 0 )
+    return ERROR_CODE;
+    
+  float concentrationPercentage = (voltage * 0.21 / 2.0) * 100;
   Serial.print("Concentration: ");
-  Serial.print(concentration);
-  Serial.println("ppm");
-  //float concentrationPercentage = concentration * 100;
-  return concentration;
+  Serial.print(concentrationPercentage);
+  Serial.println("%");
+  return concentrationPercentage;
 }
 
 
@@ -36,7 +42,7 @@ void setup(){
 
 
 void loop(){
-  getO2Concentration(A5);
+  getO2Concentration(ADC_PORT);
 }
 
 
