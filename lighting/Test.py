4b819887f99573dbf6d56b93b41d56bb7b17a245
Thomas <trichmond@vigiltechnologies.com>
2017-11-18 14:06:26 -0800

    Updated Pi code again

--- lighting/Test.py
+++ lighting/Test.py
@@ -9,45 +9,69 @@
 # In the event of an error, lights may flash. This has yet to be implemented.
 
 import os
+import time
 
+#Some constant
+ON = 1
+OFF = 0
 
-
-# The LightScheme object contains data about how to turn lights on and off1
+# The LightScheme dict object contains data about how to turn lights on and off.
+# It is limited to keys defined in the scheme_keys variable
 class LightScheme(dict):
 	
 	#ALL VALID LIGHT SCHEME KEYS MUST BE INCLUDED IN THE ARRAY BELOW
-	scheme_keys = ["L1", "L2", "L3", "L4"]
+	_scheme_keys = ["OVERHEAD_1", "OVERHEAD_2", "DOOR_COLN1", "DOOR_MARS1"]
 	
 	def __init__(self, val_type = int):
-		for key in LightScheme.scheme_keys:
+		for key in LightScheme._scheme_keys:
 			self[key] = val_type()
 		
 	#Only sets the item if it is a valid LightScheme item
 	def __setitem__(self, key, val):
-		if key not in LightScheme.scheme_keys:
+		if key not in LightScheme._scheme_keys:
 			raise KeyError
 		dict.__setitem__(self, key, val)
 
 		
-#
+counter = 0
 def generateLightScheme():
 	lightScheme = LightScheme()
-	# Check if the passed in errorCode.
-		# If the errorCode supports a valid error, return error to flash the lights 3 times.
+	
+	lightScheme["OVERHEAD_1"] = ON
+	#PIR_data = get PIR sensor data
+	#door_data = get door data
+	
+	#if PIR_data == person in room
 		
-	lightScheme["L3"] = 1
-	# Get PIR sensor data
-		#If somebody is in range of the PIR sensor, return lights on scheme.
-
-	#If nobody is detected, check the time since somebody was last seen.
-		#If it is greater than the time limit, return lights off scheme.
+		#if door_data = closed
+			#Generate scheme for bright lights
+		
+		#elif door_data = open_colony
+			#Generate scheme for dim lights
+	
+		#elif door_data = open_mars	
+			#Generate scheme for bright lights
+	
+	#else
+		#if time_elapsed == 20 seconds
+			#Generate scheme for lights off
 		
 	return lightScheme
 	
 def controlLights(light_scheme = LightScheme):
 	print(light_scheme)
 	return 0
+
+counter = 0
+while counter < 100:
+	try:
+		nextScheme = generateLightScheme()
+	except KeyError:
+		print("Error in task - Invalid LightScheme key reference")
+	else:
+		controlLights(nextScheme)
+	counter += 1
 	
-print(controlLights(generateLightScheme()))
-	
+	time.sleep(0.1)
+
 os.system("PAUSE")
