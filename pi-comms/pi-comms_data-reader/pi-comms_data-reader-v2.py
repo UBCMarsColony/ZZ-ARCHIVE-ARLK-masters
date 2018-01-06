a2f6e4af9e31e535249bbd48b688000eacc33d20
Thomas <trichmond@vigiltechnologies.com>
2018-01-06 13:47:06 -0800

    Update

--- pi-comms/pi-comms_data-reader/pi-comms_data-reader-v2.py
+++ pi-comms/pi-comms_data-reader/pi-comms_data-reader-v2.py
@@ -8,7 +8,7 @@ import json
 def getDecodedJsonString(encodedJson):
 	try:
 		return json.loads(encodedJson)
-	except:
+	except ValueError as e:
 		return None
 
 #initialization
@@ -16,13 +16,19 @@ ser = serial.Serial('/dev/ttyACM1',9600)
 
 #loop
 while True:
-    next_line = ser.readline()
-	json_data = getDecodedJsonString(next_line)
-	
 	try:
+        next_line = ser.readline()
+        json_data = getDecodedJsonString(next_line)
+        
+        #Get some info
 		print("CO2: " + str(json_data["GasComposition"]["CO2"]))
 		print("O2: " + str(json_data["GasComposition"]["O2"]))
 		print("Temperature: " + str(json_data["Temperature"]))
 		print("Pressure: " + str(json_data["Pressure"]))
-	except Exception as e:
+        #get PIR sensor data
+    
+        #if its time to update lights
+            # update lights based on sensor data
+        
+    except Exception as e:
 		print("Failed to parse JSON data.\n\tStack Trace: " + e + "\n\tSkipping line...")
