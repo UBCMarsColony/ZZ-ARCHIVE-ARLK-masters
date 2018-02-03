23fc6a0a1772c908c9cdcdd63fc8533f7b0a0ee9
Thomas <trichmond@vigiltechnologies.com>
2018-02-03 12:02:56 -0800

    Renamed

---
+++ init/init.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Mars Colony Airlock Emulator by The Electrical Team!")
+parser.add_argument("--emu")
+
+parser.parse_args()
+
+print("Nice job!")
