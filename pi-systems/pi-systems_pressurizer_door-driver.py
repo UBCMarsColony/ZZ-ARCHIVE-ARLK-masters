50cd488ff19f671e80c7cdf7b69b2ba766dc1694
Thomas <trichmond@vigiltechnologies.com>
2018-02-03 11:09:49 -0800

    Added example logic flow via pseudocode for door operation

--- pi-systems/pi-systems_pressurizer_door-driver.py
+++ pi-systems/pi-systems_pressurizer_door-driver.py
@@ -1,10 +1,47 @@
 sysThread = importlib.import_module('pi-systems_service-base')
 
 class DoorThread(sysThread.Service):
+
     def thread_task(self):
         print(self.service_thread.name + " is running!")
+        
+        #WARNING: MASS PSEUDOCODE
+        #-------------------------
+        # This is an example of logic
+        # that could be used for the door.
+        #-------------------------
+        sData = getLatestSensorData()
+        
+        if sData is good:
+            move door
+            specify target switch
+            
+            while(door is running):
+            
+                #Error checks
+                if override is requested
+                    disconnect the motor 
+                    set a callback listener until the motor is reset
+                    break
+                
+                if the door is timing out
+                    get last requested door state
+                    run course of action logic
+                
+                if a sensor problem is encountered:
+                    run course of action logic
+                    
+                #Target state logic
+                if target switch is pressed:
+                    stop the door
+                    run security measures
+        close()
+
 
 def runSequence():
     dThread = DoorThread("Door Thread", 2)
+    dThread.start()
+    
+    dThread.join()
     #start new thread to open doors
         #continuously update a state variable accessible anywhere
