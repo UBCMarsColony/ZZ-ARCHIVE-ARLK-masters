5430942dcbe4306ea235167085cc764ffaae6673
Thomas <trichmond@vigiltechnologies.com>
2018-02-05 16:11:52 -0800

    Renamed "Service" to "Subsystem"

--- pi-systems/pi-systems_subsystem-base.py
+++ pi-systems/pi-systems_subsystem-base.py
@@ -6,29 +6,30 @@ The purpose of the Service class is to ease system thread management.
 In general, we would like airlock services to function under a similar
 command flow.
 """
-class Service(ABC):
+class Subsystem(ABC):
     def __init__(self, name, threadID = None):
         if(threadID = None):
             pass#generate a valid number
-            
-        self.service_thread = self.ServiceThread(self, threadID, name)
-
-    class ServiceThread(threading.Thread):
-        def __init__(self, service, threadID, name):
+        
+        self.subsystem_thread = self.ServiceThread(self, threadID, name)
+        print("Initialized new subservice:\n\tName=" + str(name) + "\n\tID=" + str(threadID))
+        
+    class SubsystemThread(threading.Thread):
+        def __init__(self, subsystem, threadID, name):
             threading.Thread.__init__(self)
-            self.service = service
+            self.subsystem = subsystem
             self.threadID = threadID
             self.name = name
             
         def run(self):
             print("Thread started:\n\t" + self.name)
-            self.service.thread_task()
+            self.subsystem.thread_task()
         
     def start(self):
-        self.service_thread.start()
+        self.subsystem_thread.start()
         
     def join(self):
-        self.service_thread.join()
+        self.subsystem_thread.join()
     
     """
     Contains the code which will be run during the threads life.
