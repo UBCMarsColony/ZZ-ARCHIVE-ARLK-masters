32f32be582e2f89db2a7d079c2fd3bf12a325662
Kevin Oesef (Object 263) <kevinoesef@gmail.com>
2018-03-04 07:20:14 +0900

    Finalized door opening and closing code with datum etc. Works great, need to port to python.

--- motors/stepper-motor/stepper-motor.ino
+++ motors/stepper-motor/stepper-motor.ino
@@ -1,122 +1,161 @@
 //constants
 const int pinPUL_low = 11;
-const int pinDIR_low = 13;
-const int pinENA_low = 999;
-const int pinSEN_open = ;
-const int pinSEN_closed = ;
+const int pinDIR_low = 12;
+const int pinENA_low = 13;
+const int pinSEN_open = 10;
+const int pinSEN_closed = 9;
 
 const int gearRatio = 47;
+const int pulseWidth = 2;
 const int ON = 1;
 const int OFF = 0;
 
-const int INDETERMINATE = 404
-const int CLOSED = 500
-const int OPEN = 600
-const int TRANSIT = 700
+const int INDETERMINATE = 404;
+const int CLOSED = 500;
+const int OPEN = 600;
+const int TRANSIT = 700;
 
 int currentAngle;
 int datumClosed;
 int datumOpen;
-int doorStatus=INDETERMINATE;
+int doorStatus;
+int shake;
 
 void setup() {
-  
-  //pin mode setups
-  pinMode(PULpinH, OUTPUT);
-  pinMode(pinPUL_low, OUTPUT);
-  pinMode(DIRpinH, OUTPUT);
-  pinMode(pinDIR_low, OUTPUT);
 
-  //initial conditions for motor
-  digitalWrite(DIRpinH, HIGH);
-  digitalWrite(pinDIR_low, LOW);
+    //pin mode setups
+    pinMode(pinPUL_low, OUTPUT);
+    pinMode(pinDIR_low, OUTPUT);
+    pinMode(pinSEN_closed, INPUT_PULLUP);
+    pinMode(pinSEN_open, INPUT_PULLUP);
 
-  digitalWrite(PULpinH, HIGH);
-  digitalWrite(pinPUL_low, LOW);
-  Serial.begin(9600);
+    //initial conditions for motor
+    digitalWrite(pinDIR_low, LOW);
+    digitalWrite(pinPUL_low, LOW);
+    digitalWrite(pinENA_low, LOW);
+    Serial.begin(9600);
+
+    //statuses
+    doorStatus=INDETERMINATE;
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //starting definitions
-  char rotateDirection='L';
-  int rotateAngle=90;
+    // put your main code here, to run repeatedly:
+    //starting definitions
+    int rotateAngle=2;
 
-  stepperAngleRotate(rotateAngle,'L');
-  delay(3000);
-  stepperAngleRotate(rotateAngle,'R');
-  delay(3000);
+    // motorPower(ON);
+    shake=0;
+    while(shake<4){
+        stepperAngleRotate(rotateAngle,'R');
+        stepperAngleRotate(rotateAngle,'L');
+        shake++;
+        Serial.println("Shaking!");
+    }
+    delay(2000);
+    doorOpen();
+    delay(5000);
+    doorClose();
 
 }
 
 //motor is low side switching! 5V from arduino 5v rail. logical disable is pulling the L pin to 5V.
 
-void doorCalibratePosition(void){
-
-}
-
-void doorOpen(void){
-    if(doorStatus!=CLOSED){
-        while(digitalRead(pinSEN_closed==LOW)){
-            stepperAngleRotate(90,'R');
-            delay(3000);
-            doorStatus=CLOSED;
-        }
-    }
-    else
-        if(doorStatus==OPEN){
-            break;
-    }
-    else 
-        while(digitalRead(pinSEN_open==LOW){
-            stepperAngleRotate(90,'L');
-            delay(3000);
-            doorStatus=OPEN;
-    }
-}
-
-void doorClose(void){
-    if(doorStatus==OPEN){
-        break;
-    }
-    else 
-        while(digitalRead(pinSEN_closed==LOW){
-            stepperAngleRotate(90,'R');
-            delay(3000);
-            doorStatus=OPEN;
-    }
-}
-
 //function stepperAngleRotate takes int angle, char direction, rotates angle degrees in specified direction. 
 void stepperAngleRotate(int angle, char direction){
-  
-  //defins
-  int index=0;
-  float stepsToRevCurrentMode=400.0; //this is per stepper raw rotation, not gearboxed rotation
-  float requiredPulses=(angle*gearRatio/360.0)*stepsToRevCurrentMode;
 
-  //direction switching routine
-  if (direction=='R'){
-    digitalWrite(pinDIR_low,HIGH);
-  }
-  else{pinDIR_low
-    digitalWrite(DIRpinL,LOW)
+    //defins
+    int index=0;
+    float stepsToRevCurrentMode=400.0; //this is per stepper raw rotation, not gearboxed rotation
+    float requiredPulses=(angle*gearRatio/360.0)*stepsToRevCurrentMode;
+
+    //direction switching routine
+    if (direction=='R'){
+        digitalWrite(pinDIR_low,HIGH);
+    }
+    else{
+        digitalWrite(pinDIR_low,LOW);
   }
 
-  //beware of low side switching, pinPUL_low,HIGH); //STOPPING PULSE LOW
-    delay(2);
-    
-    index++;
-  }
+    //pulse generator routine
+    while(index<=requiredPulses){
+        //beware of low side switching, pinPUL_low,HIGH); //STOPPING PULSE LOW
+        digitalWrite(pinPUL_low, HIGH);
+        delay(pulseWidth);
+        digitalWrite(pinPUL_low, LOW);
+        delay(pulseWidth);
+        // Serial.print("This is increment: ");
+        // Serial.print(index);
+        // Serial.print(" out of: ");
+        // Serial.println(requiredPulses);
+        index++;
+    }
+}
+
+//increments the stepper motor by one tick. Can be used for a different function that tracks angle.
+void stepperAngleIncrement(char direction){
+    digitalWrite(pinDIR_low,direction);
+    digitalWrite(pinPUL_low,LOW);
+    delay(pulseWidth);
+    digitalWrite(pinPUL_low,HIGH);
+    delay(pulseWidth);
+}
+
+int doorOpen(void){
+
+    Serial.println("Open commanded");
+    delay(1000);
+
+    if(doorStatus!=CLOSED){
+        while(digitalRead(pinSEN_closed)==HIGH){
+            stepperAngleIncrement('R');
+            doorStatus=TRANSIT;
+            Serial.println("Closing!");
+        }
+        doorStatus=CLOSED;
+        Serial.println("Door closed!");
+    }
+
+    while(digitalRead(pinSEN_open)!=LOW){
+        stepperAngleRotate(1,'L');
+        doorStatus=TRANSIT;
+        Serial.println("Door opening!");
+    }
+    doorStatus=OPEN;
+    Serial.println("Door opened!");
+    return;
+}
+
+int doorClose(void){
+
+    Serial.println("Close commanded");
+    delay(1000);
+
+    if(doorStatus==CLOSED){
+        Serial.println("Door already closed!");
+        return 0;
+    }
+    while(digitalRead(pinSEN_closed)!=LOW){
+        stepperAngleIncrement('R');
+        doorStatus=TRANSIT;
+        Serial.println("Door closing!");
+    }
+    doorStatus=CLOSED;
+    Serial.println("Door closed!");
 }
 
 //Function motorPower takes integer status and switches the motor on or off
 void motorPower(int status){
-  if (status==ON){
-    digitalWrite(pinENA_low,HIGH)
-  }
+    if(status==ON){
+        digitalWrite(pinENA_low,LOW);
+    }
+    else{
+        digitalWrite(pinENA_low,HIGH);
+    }
 }
 
-int modeLUT(string mode){
-    return stepsToRev_raw
-}
+// int modeLUT(string mode){
+//     enum motorMode{'0111'=400,'1011'=800,'0011'=1600}
+//     return stepsToRev_raw
+// }
