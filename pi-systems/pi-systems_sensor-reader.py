ef1ec4bafb041453df53a3de63a067076ca6859d
Thomas <trichmond@vigiltechnologies.com>
2018-03-31 11:10:51 -0700

    Converted data reader to a subsystem, sensor-reader

--- pi-systems/pi-systems_sensor-reader.py
+++ pi-systems/pi-systems_sensor-reader.py
@@ -1,50 +1,58 @@
-# JSON raspberry pi data reader code, take serial data from python
+subsys = importlib.import_module('pi-systems_subsystem-base')
 
 try:
     import serial
 except ModuleNotFoundError:
     print("Could not import serial.")
+
 import json
-import time
+    
+# JSON raspberry pi data reader code, take serial data from python
 
-# THE VARIABLE THAT MATTERS
-__sensor_data = {}
+class SensorSubsystem(subsys.Subsystem):
+    
+    __sensor_data = {}
+    
+    def __init__(self, gpio, name=None, threadID=None)
+        
+        self.serial_in = serial.Serial('/dev/ttyACM1',9600)
+        
+        super().__init__(gpio, name, threadID)
+    
+     def get_data(self, string_name = None):
+        if string_name is None:
+            return SensorSubsystem.__sensor_data
+        else:   
+            try:
+                return SensorSubsystem.__sensor_data[string_name]
+            except KeyError as ve:
+                print("Failed to find a value associated with the key " + string_name + ". Returning entire dictionary instead...")
+                return SensorSubsystem.__sensor_data
+            except Exception as e:
+                print("An unexpected exception occurred while trying to retrieve sensor data.\n\tStack Trace: " + str(e))
+        
+        return None
+    
+    
+    def thread_task(self):
+        self.__update_sensor_data()
+    
 
-try:
-    serial_conn = serial.Serial('/dev/ttyACM1',9600)
-except NameError:
-    pass
+    def __update_sensor_data(self):
+        try:    
+            next_line = self.serial_in.readline()
+            
+            SensorSubsystem.__sensor_data = __get_decoded_json_string(next_line)
 
-def get_sensor_data(string_name = None):
-    if string_name is None:
-        return __sensor_data
-    else:   
-        try:
-            return __sensor_data[string_name]
-        except KeyError as ve:
-            print("Failed to find a value associated with the key " + string_name + ". Returning entire dictionary instead...")
-            return __sensor_data
+        except ValueError as ve:
+            print("Failed to parse JSON data.\n\tStack Trace: " + str(ve) + "\n\tSkipping line...")
         except Exception as e:
-            print("An unexpected exception occurred while trying to retrieve sensor data.\n\tStack Trace: " + str(e))
+            print("An unexpected exception occurred while trying to update Pi sensor data. \n\tStack Trace: " + str(e))
+            
     
-    return None
-
-    
-def update_sensor_data():
-    try:    
-        next_line = serial_conn.readline()
-        global __sensor_data
-        __sensor_data = __get_decoded_json_string(next_line)
-
-    except ValueError as ve:
-        print("Failed to parse JSON data.\n\tStack Trace: " + str(ve) + "\n\tSkipping line...")
-    except Exception as e:
-        print("An unexpected exception occurred while trying to update Pi sensor data. \n\tStack Trace: " + str(e))
-
-
-# function that securely decodes a JSON string
-def __get_decoded_json_string(encoded_json):
-    try:
-        return json.loads(encoded_json)
-    except ValueError:
-        raise ValueError
+    # function that securely decodes a JSON string
+    def __get_decoded_json_string(self, encoded_json):
+        try:
+            return json.loads(encoded_json)
+        except ValueError:
+            raise ValueError
