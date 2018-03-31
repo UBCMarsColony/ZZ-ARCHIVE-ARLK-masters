bc344d2a96e0608e0cdb505af1ae18246f15217d
Thomas <trichmond@vigiltechnologies.com>
2018-03-31 13:30:06 -0700

    Implemented basic functionality

--- pi-systems/pi-systems_input-manager.py
+++ pi-systems/pi-systems_input-manager.py
@@ -3,12 +3,21 @@ subsys = importlib.import_module('pi-systems_subsystem-base')
 
 class InputManager(subsys.Subsystem):
     
+    _input_pins = [12,13,14,15] #TODO actually assign proper pins
+    
     def thread_task(self):
-        print("R-r-r-r-r-running!")
+        while self.is_running():
+            print(str(check_buttons()))
+            
         
     def check_buttons(self):
-        pass
-        #IMPLEMENT ME
+        for pin in InputManager._input_pins:
+            if self.gpio.input(pin)
+                return pin
+        
+        return None
+        
         
     def send_instructions(self):
+        #Implement me
         pass
