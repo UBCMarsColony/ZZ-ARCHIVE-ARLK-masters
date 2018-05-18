17688b2607db354f48769d8c54445843512a8ec6
koesef@toughbook <kevinoesef@gmail.com>
2018-05-18 16:12:44 -0700

    Door control with string copy and handling, working using cJSON parser to parse our incoming JSON stream.

--- door-control-box/door-control-box.ino
+++ door-control-box/door-control-box.ino
@@ -1,15 +1,35 @@
 //constants and libs
 #include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+#include <SPI.h>
+#include <../../cJSON/cJSON.h>
 
 SoftwareSerial COML0(10,11); //(Rx,Tx)
 
+double env_conc.O2;
+double env_conc.CO2;
+double env_temp;
+double env_pres;
+int stringLength_required; //beware of global variables
+char serialInput_raw[512]; //beware of global variables
+
 void setup(){
- Serial.begin(9600);
- COML0.begin(9600);// begin logical serial port COML0
+ Serial.begin(9600);// begin physical uart comms via USB
+ COML0.begin(9600);// begin logical serial port COML0 at pins above
 }
 
 void loop(){
+    //conditioning string for the cJSON parser
+    stringLength_required=stringReader(serialInput_raw[]);
+    char serialInput_forParser[stringLength_required+1];
+    strncpy
+    // int stringLength_index=0;
+    // for(stringLength_index<stringLength_required){ //copy string char by char
+    //     serialInput_forParser[stringLength_index]=serialInput_forParser[stringLength_index];
+    // }
+    // serialInput_forParser[stringLength_index+1]='\0'; //string terminator with null
 
+    
 }
 
 //self-defined functions
@@ -19,7 +39,7 @@ void loop(){
     RETURNS: number of bytes written to stringInput
     FUNCTION: reads a single data frame from serial, enclosed by start character frame_start and end character frame_end
 */
-int stringReader(stringInput){
+int stringReader(char stringInput[]){
 
     char COM_readChar;
     char frame_start="\a"
