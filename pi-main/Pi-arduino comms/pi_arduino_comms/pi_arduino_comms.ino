a184e94156e93bfb71b65085bec92cb098e723aa
Jackyruth <jackyjiang98@gmail.com>
2018-07-14 12:00:14 -0700

    savepoint

--- pi-main/Pi-arduino comms/pi_arduino_comms/pi_arduino_comms.ino
+++ pi-main/Pi-arduino comms/pi_arduino_comms/pi_arduino_comms.ino
@@ -1,10 +1,14 @@
 #include <Wire.h>
+#include <string.h>
 
 #define SLAVE_ADDRESS 10
-
+typedef unsigned char   BYTE;
 int number = 0;
 int state = 0;
 
+bool ready_flag = false;
+String send_string;
+BYTE send_val;
 void setup()
 {
     //Serial.begin(9600);
@@ -24,8 +28,41 @@ void setup()
 
 }
 void loop()
-{
+{   BYTE c;
+    send_string = "There, out in the darkness, a fuggggitive running, fallen from God";
+    //ready_flag = false;
     delay(100);
+
+//TESTING, not needed for actual program
+    String ttt = "hello, my name is elder cummingham!";
+    BYTE send_byte;
+    int len = ttt.length();
+
+    for(int i = 0;  i < len ; i++){
+        send_byte = byte(ttt[i]);
+        Serial.println(ttt[i]);
+        Serial.print(send_byte);
+    }
+    /*      THE INITIAL "ECHO" CODE
+    // send data only when you receive data:
+    if (Serial.available() > 0) {
+            // read the incoming byte:
+            c = Serial.read();
+
+            // say what you got:
+            Serial.print("I received: ");
+            Serial.println(c, DEC);
+    }
+    if(c){
+        send_val = byte(c);
+        ready_flag = true;
+        Serial.print("The byte value is");
+        Serial.println((char)send_val);
+    }
+    */
+
+
+    delay(1000);
 }
 
 void receiveData(int byteCount)
@@ -35,6 +72,7 @@ void receiveData(int byteCount)
         number = Wire.read();
         Serial.print("data received: ");
         Serial.println(number);
+        /*
         if (number == 1)
         {
             if (state == 0)
@@ -48,11 +86,22 @@ void receiveData(int byteCount)
             state = 0;
             }
         }
+        */
     }
 }
 
 // callback for sending data
 void sendData()
 {
-    Wire.write(number);
+    //Wire.write(send_string);      //send entire string option
+    //BYTE BY BYTE OPTION
+    BYTE send_byte;
+    int len = send_string.length();
+
+    for(int i = 0;  i < len ; i++){
+        send_byte = send_string[i];
+        Serial.println(send_byte);
+        Wire.write(send_byte);
+    }
+    //Wire.write(number);      
 }
