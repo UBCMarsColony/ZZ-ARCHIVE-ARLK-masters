2fb7548e5800db3551392ac83cdc1373d9b22c08
Jackyruth <jackyjiang98@gmail.com>
2018-09-15 11:35:34 -0700

    Everything works but I2c not implemented yet

--- Pressurization/pressurization_procedure/pressurization_procedure.ino
+++ Pressurization/pressurization_procedure/pressurization_procedure.ino
@@ -26,22 +26,30 @@
 #define button_marsside         3
 
 //LED display
-#define status_press            0
-#define status_progress         1
-#define status_depress          2
+#define status_press            4
+#define status_progress         5
+#define status_depress          6
 
 //DEBUGGING definitions
-#define pressure_data           101.3 // in kpa
+float pressure_data = 0; // in kpa
 
 ////////////////// Get State from Pressure data from Pi/////////////////////
 byte get_airlock_state(int pressure);
 
 ///////////////// Procedure and primitive definitions////////
+byte cmd_sel;
 void procedure();
 bool hold_press();
 bool DPR();
 bool PR();
-
+void colonyside(){
+    cmd_sel = 'o';
+    //procedure(exit);
+}
+void marside(){
+    cmd_sel = 'i';
+    //procedure(entry);
+}
 //////////////// Display driver //////////////////////////
 int led_status_display();
 
@@ -60,6 +68,8 @@ void setup(){
     Serial.begin(9600);
 
     //////////Button and interrupts////////
+    //procedure flag
+    cmd_sel = 'x';
     //There's no software debouncing, so build button debouncing circuits
     pinMode(button_colonyside, INPUT_PULLUP);
     pinMode(button_marsside, INPUT_PULLUP);
@@ -70,39 +80,51 @@ void setup(){
     pinMode(status_depress,OUTPUT);
 
     //Button interrupts
-    attachInterrupt(digitalPinToInterrupt(button_colonyside), colonyside, FALLING);
-    attachInterrupt(digitalPinToInterrupt(button_marsside), marside, FALLING); 
+    attachInterrupt(digitalPinToInterrupt(button_colonyside), colonyside, RISING);
+    attachInterrupt(digitalPinToInterrupt(button_marsside), marside, RISING); 
     u_airlock.valve_a = initial_valve_a;
     u_airlock.valve_b = initial_valve_b;
 
 
-}
-void colonyside(){
-    procedure(exit);
-}
-void marside(){
-    procedure(entry);
+
 }
 void loop() {
     //Looping code 
     u_airlock.pressure = pressure_data;
     u_airlock.state = get_airlock_state(u_airlock.pressure);
+
+    Serial.print("Pressure is:\t");
+    Serial.println(u_airlock.pressure);
+    Serial.print("State is:\t");
+    Serial.println(u_airlock.state);
+    Serial.print("cmd_sel is:\t");
+    Serial.println(char(cmd_sel));
+    if(cmd_sel == 'i'){
+        procedure(entry);
+    }
+    else if (cmd_sel == 'o'){
+        procedure(exit);
+    }
+    cmd_sel = 'x';
     led_status_display(u_airlock.state);
     delay(1000);
 
 }
 int led_status_display(byte state){
-    if(state == status_press){
+    if(state == pressurize){
+        Serial.println("Pressruized");
         digitalWrite(status_press, HIGH);
         digitalWrite(status_depress, LOW);
         digitalWrite(status_progress, LOW);
     }
-    else if (state == status_depress){
+    else if (state == depressurize){
+        Serial.println("dePressruized");
         digitalWrite(status_press, LOW);
         digitalWrite(status_depress, HIGH);
         digitalWrite(status_progress, LOW);
     }
-    else if (state == status_progress){
+    else if (state == in_progress){
+        Serial.println("inprogress");
         digitalWrite(status_press, LOW);
         digitalWrite(status_depress, LOW);
         digitalWrite(status_progress, HIGH);
@@ -111,16 +133,20 @@ int led_status_display(byte state){
 }
 //Entering the airlock from mars is called entry, and leaving the airlock called exit
 void procedure(bool select_procedure){
-    if(select_procedure == entry){
+    if(select_procedure == exit){
         PR();
         Serial.println("Opening colony -> airlock door");
+        Serial.println("Detect airlock door close");
         DPR();
         Serial.println("Opening airlock -> mars door");
+        Serial.println("Detect mars door close");
     }
-    else if(select_procedure == exit){
+    else if(select_procedure == entry){
         Serial.println("Opening airlock -> mars door");
+        Serial.println("Detect mars door close");
         PR();
         Serial.println("Opening colony -> airlock door");
+        Serial.println("Detect airlock door close");
         DPR();
     }
 }
@@ -130,6 +156,7 @@ bool PR(){
     while(u_airlock.state != pressurize){
         u_airlock.valve_a = close;
         u_airlock.valve_b = open;
+        u_airlock.state = get_airlock_state(pressure_data++);
     }
     hold_press();
 }
@@ -137,6 +164,7 @@ bool DPR(){
     while(u_airlock.state != depressurize){
         u_airlock.valve_a = open;
         u_airlock.valve_b = close;
+        u_airlock.state = get_airlock_state(pressure_data--);
     }
     hold_press();
 }
@@ -149,11 +177,13 @@ bool hold_press(){
 
 byte get_airlock_state(int pressure){
     byte state;
-    if (mars_atm - epsilon < pressure < mars_atm + epsilon){
-        state = depressurize;//0
+    Serial.print("Inside Get airlock state");
+    Serial.println(pressure);
+    if (mars_atm - epsilon < pressure && pressure < mars_atm + epsilon){
+        state = depressurize;
     }
-    else if(one_atm - epsilon < pressure < one_atm + epsilon)
-        state = pressurize;//1
+    else if(one_atm - epsilon < pressure && pressure < one_atm + epsilon)
+        state = pressurize;
     else
         state = in_progress;
     return state;
