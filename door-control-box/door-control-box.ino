2200998c5e3c45686582cc62e975b6e2dec62111
Kevin Oesef (Object 263) <kevinoesef@gmail.com>
2018-09-23 02:29:29 +0900

    Controller test

--- door-control-box/door-control-box.ino
+++ door-control-box/door-control-box.ino
@@ -2,87 +2,32 @@
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
 #include <SPI.h>
-//#include <../../cJSON/cJSON.h>
-
-//SoftwareSerial COML0(10,11); //(Rx,Tx)
-
 #include <cJSON.h>
 
 #define pin_RX 10
 #define pin_TX 11
 
+const int ID_device=46; //MUST BE IN HEX
 
-SoftwareSerial COML0(pin_RX,pin_TX); //(pin for Rx,Tx)
-
-double env_conc_O2;
-double env_conc_CO2;
-double env_temp;
-double env_pres;
+typedef struct env;
+struct env{
+    double conc_O2;
+    double conc_CO2;
+    double temperature;
+    double pressure;
+    char door;
+    char pump;
+};
 
 void setup(){
-Serial.begin(9600);// begin physical uart comms via USB
-COML0.begin(9600);// begin logical serial port COML0 at pins above
+    Serial.begin(9600);
+    Wire.begin(ID_device);
 }
 
 void loop(){
-    //conditioning string for the cJSON parser
-    //stringLength_required=stringReader(serialInput_raw[]);
-    Serial.println("Program start!");
-    // set vars
-    int stringLength_index=0;
-    int stringLength_required; //beware of global variables
-    char serialInput_raw[1024]="{\"title\":\"Person\",\"type\":\"object\",\"properties\":{\"firstName\":{\"type\":\"string\"},\"lastName\":{\"type\":\"string\"},\"age\":{\"description\":\"Age in years\",\"type\":\"integer\",\"minimum\": 0}},\"required\":[\"firstName\", \"lastName\"]}";
-
-    Serial.print("JSON string to be decoded is: ");
-    Serial.println(serialInput_raw);
-
-    stringLength_required=strlen(serialInput_raw);
-
-    char serialInput_forParser[stringLength_required+1];
-
-    for(stringLength_index=0;stringLength_index<stringLength_required;stringLength_index++){ //copy string char by char
-        serialInput_forParser[stringLength_index]=serialInput_raw[stringLength_index];
-    }
-    serialInput_forParser[stringLength_index+1]='\0'; //string terminator with null
-    
-    Serial.print("JSON string on smaller array is: ");
-    Serial.println("serialInput_forParser");
-    
-    delay(1000);
-
+    Wire.onReceive(inputHandler);
 }
 
-//self-defined functions
+void inputHandler(howMany){
 
-/* void stringReader(void)
-    TAKES: reference to string stringInput of predetermined length
-    RETURNS: number of bytes written to stringInput
-    FUNCTION: reads a single data frame from serial, enclosed by start character frame_start and end character frame_end
-*/
-int stringReader(char stringInput[]){
-
-    char COM_readChar;
-    char frame_start="\a";
-    char frame_end="\n";
-    
-    int readLength=0;
-    int TRIGGER=0;//fuse function (?)
-    
-    //does blank reads from strim until alert character \a is found. fused so it runs at most once
-    while(COM_readChar!=frame_start && TRIGGER!=1){
-        COM_readChar=COML0.read(); //run continuously to discard from strim
-        if(COM_readChar==frame_start){
-            TRIGGER=1; //fuse if frame_start detected
-            break;
-        }
-    }
-
-    //read until end of frame
-    while(COM_readChar!=frame_end){
-        COM_readChar=COML0.read();
-        stringInput[readLength]=COM_readChar;
-        readLength++;
-    }
-
-    return readLength;
 }
