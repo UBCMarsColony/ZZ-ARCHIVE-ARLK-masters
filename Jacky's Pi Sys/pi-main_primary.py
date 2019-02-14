3f278397c372100df2f14370242d7a77b83fc019
Thomas <trichmond@vigiltechnologies.com>
2019-02-13 17:52:20 -0800

    Linted main looper

--- Jacky's Pi Sys/pi-main_primary.py
+++ Jacky's Pi Sys/pi-main_primary.py
@@ -16,52 +16,66 @@ pressure_ss = importlib.import_module('pi-systems_pressure-manager')
 door_ss = importlib.import_module('pi-systems_door-subsystem')
 
 """
-Purpose: Performs initial system setup and begins airlock loop cycle. Handles any breakouts within the loop cycle.
-Parameter: runtime_params - The Namespace returned by the argument parser in init.py
+Purpose: Performs initial system setup and begins airlock loop cycle.
+    Handles any breakouts within the loop cycle.
+Parameter: runtime_params - The Namespace returned by the argument parser
+    in init.py
 """
+
+
 def begin(runtime_params):
-    start_time = time() 
+    start_time = time()
     print("\n\n---INITIALIZING AIRLOCK SYSTEMS---")
-    
+
     # Start initializing the vital airlock systems
     subsystems = []
 
     # subsystems.append(sensor_ss.SensorSubsystem(
-    #         name="airlock1_sensors", thread_id=0xDE7EC7, address=0x0A))    
+    #   name="airlock1_sensors",
+    #   thread_id=0xDE7EC7,
+    #   address=0x0A))
 
-    # subsystems.append(lights_ss.LightingSubsystem(name="airlock1_lights-internal", thread_id=0x5EE, pins=18))
-
+    # subsystems.append(lights_ss.LightingSubsystem(
+    #   name="airlock1_lights-internal",
+    #   thread_id=0x5EE,
+    #   pins=18))
 
     # door_col = door_ss.DoorSubsystem(
-    #     name="airlock1_door_col", thread_id=0xD00121, address=0)
+    #   name="airlock1_door_col",
+    #   thread_id=0xD00121,
+    #   address=0)
     # subsystems.append(door_col)
 
-
-    subsystems.append(pressure_ss.PressureSubsystem(name="airlock1_pressurization", thread_id=0xAE120))
+    subsystems.append(pressure_ss.PressureSubsystem(
+        name="airlock1_pressurization",
+        thread_id=0xAE120))
     #     door_input_ss.DoorInputSubsystem(
-    #         name="airlock1_doorinput_col", thread_id=0xC01, address="FILL ME IN", linked_door=door_col),
+    #         name="airlock1_doorinput_col",
+    # thread_id=0xC01, address="FILL ME IN", linked_door=door_col),
     # )
 
     # door_mars = door_ss.DoorSubsystem(
-    #         name="airlock1_door_mars", thread_id=0xD00122, address="FILL ME IN")
+    #   name="airlock1_door_mars",
+    #   thread_id=0xD00122,
+    #   address="FILL ME IN")
+    #
     # subsystems.append(
-    #     door_mars,
-    #     door_input_ss.DoorInputSubsystem(
-    #         name="airlock1_doorinput_mars", thread_id=0x12ED, address="FILL ME IN", linked_door=door_mars)
-    # )
-    
-    
-    
-    
+    #   door_mars,
+    #   door_input_ss.DoorInputSubsystem(
+    #       name="airlock1_doorinput_mars",
+    #       thread_id=0x12ED,
+    #       address="FILL ME IN",
+    #       linked_door=door_mars))
+
     # input = input_ss.InputSubsystem("input", 5)
     # input.start()
-    
+
     # valves = valve_ss.PressureSubsystem("valves", 6)
     # valves.start()
-    
+
     # lights = light_ss.LightingSubsystem("lights", 4)
-    # lights.start() 
-    
+    # lights.start()
+
     print("---AIRLOCK SYSTEMS INITIALIZED---\n")
 
     print("---STARTING SUBSYSTEMS---")
@@ -69,17 +83,21 @@ def begin(runtime_params):
         try:
             subsystem.start()
         except Exception as e:
-            print("WARNING: Subsystem could not start as due to an unexpected exception:\n\t", e)
-    
+            print(
+                "WARNING: Subsystem could not start as due to an" +
+                "unexpected exception:\n\t", e)
+
     print("\n---ALL SUBSYSTEMS STARTED---")
 
     keyboard.on_press(handle_cmd)
     print("\n---COMMAND INPUT ENABLED---\n.")
 
-    print("\n---AIRLOCK SETUP COMPLETE.---\nElapsed Setup Time: %i" % (start_time - time()))
-    
+    print(
+        "\n---AIRLOCK SETUP COMPLETE.---\nElapsed Setup Time: %i"
+        % (start_time - time()))
+
     print("\n---STARTING LOOPER SEQUENCE---\n")
-    
+
     while True:
         try:
             loop(runtime_params)
@@ -91,7 +109,7 @@ def begin(runtime_params):
                 break
             else:
                 print("Airlock shutdown cancelled")
-        
+
 
 def loop(runtime_params):
     pass
@@ -104,32 +122,38 @@ def handle_cmd(cmd):
     # Door Toggles
     if cmd is 'o' or cmd is 'O':
         print("Requesting door open")
-        subsystems["airlock1_doors"].request_door_state(subsystems["airlock1_doors"].Procedure.OpenDoor)
+        subsystems["airlock1_doors"].request_door_state(
+            subsystems["airlock1_doors"].Procedure.OpenDoor)
     elif cmd is 'c' or cmd is 'C':
         print("Requesting door close")
-        subsystems["airlock1_doors"].request_door_state(subsystems["airlock1_doors"].Procedure.CloseDoor)
+        subsystems["airlock1_doors"].request_door_state(
+            subsystems["airlock1_doors"].Procedure.CloseDoor)
 
     # Pressure Toggles
     elif cmd is 'p' or cmd is 'P':
         print("Pressurizing...")
-        subsystems['airlock1_pressurization'].request_new_state(subsystems['airlock1_pressurization'].TargetState.Pressurize)
+        subsystems['airlock1_pressurization'].request_new_state(
+            subsystems['airlock1_pressurization'].TargetState.Pressurize)
 
     elif cmd is 'd' or cmd is 'D':
-        subsystems['airlock1_pressurization'].request_new_state(subsystems['airlock1_pressurization'].TargetState.Depressurize)
+        subsystems['airlock1_pressurization'].request_new_state(
+            subsystems['airlock1_pressurization'].TargetState.Depressurize)
         print("Depressurizing")
 
     # Sensors SS Debugging
     elif cmd is 's':
         with subsystems["airlock1_sensors"] as sensors:
             sensors.print_updates = not sensors.print_updates
-            print('Sensors printout %s' % ("enabled" if sensors.print_updates else "disabled"))
+            print(
+                'Sensors printout %s'
+                % ("enabled" if sensors.print_updates else "disabled"))
 
     # Light SS Debugging
     elif cmd is 'l':
         with subsystems['airlock1_lights-internal'] as lights:
             lights.toggle()
             print('\nLights %s' % ('on' if lights.light_state else 'off'))
-    
+
     # General System Information/
     elif cmd is "i":
         print("Current subsystem pool items:\n---------\n")
@@ -139,15 +163,16 @@ def handle_cmd(cmd):
         print("Current subsystem pool items:\n---------\n")
         print(repr(subsystems))
     elif cmd is '?':
-        print("----- KEYBOARD COMMANDS -----" +
+        print(
+            "----- KEYBOARD COMMANDS -----" +
             "\no: Request door open\nc: Request door close" +
-            "\nl: Toggle Lights" + 
+            "\nl: Toggle Lights" +
             "\ns: Toggle sensor debug printing" +
             "\ns: Print sensor subsystem updates" +
             "\ni: Show items in subsystem pool" +
             "\nI: Show details of items int the subsystem pool" +
             "#n!: Stop Colony" +
-            "\n?: Help window (this text)" + 
+            "\n?: Help window (this text)" +
             "\n-------------------")
     elif cmd is '!':
         print('---STOPPING COLONY---')
@@ -160,7 +185,7 @@ def handle_cmd(cmd):
     # elif nextinput == "c" or nextinput == "C":
     #     print("Requesting door close")
     #     subsystems["airlock1_doors"].request_door_state(subsystems["airlock1_doors"].Procedure.CloseDoor)
-    
+
     # # SUBSYSTEMS INFO
     # elif nextinput == "i" or nextinput == "I":
     #     print("Current subsystem pool data:\n---------\n")
@@ -168,75 +193,34 @@ def handle_cmd(cmd):
     # elif nextinput == 's' or nextinput == 'S':
     #     with subsystems["airlock1_sensors"] as sensors:
     #         sensors.print_updates = not sensors.print_updates
-    #         print('Sensors printout %s' % ("enabled" if sensors.print_updates else "disabled"))
-    
+    #         print(
+    #           'Sensors printout %s'
+    #           % ("enabled" if sensors.print_updates else "disabled"))
+
     # # CLI INFO
     # elif nextinput == '?':
-    #     print("----- KEYBOARD COMMANDS -----\no: Request door open\nc: Request door close" +
+    #     print(
+    #       "----- KEYBOARD COMMANDS -----" +
+    #       "\no: Request door open\nc: Request door close" +
     #         "\ns: Print sensor subsystem updates" +
-    #         "\ni: Print contents of the subsystem pool\n?: Help window (this text)" + 
+    #         "\ni: Print contents of the subsystem pool" +
+    #         "\n?: Help window (this text)" +
     #         "\n-------------------")
     # else:
     #     print("Command not recognized")
-
-
 
     ######
     # TODO find a nicer way to do this
     # subsystems = ss_pool.get_all()
     # data = subsystems["sensors"].get_data()
     # next_button = subsystems["input"].check_buttons()
-    
+
     # if next_button == 16:
     #     #This is placeholder code
     #     subsystems["valves"].request_new_state(valve_ss.PressureSubsystem.std_state["close"])
     #     pass
-    
+
     # subsystems["lights"].input_data(data)
     # #input.update_UI()
-    
+
     # time.sleep(runtime_params.loop_delay)
-
-        
-"""
-INIT
-----
-
-
-NORMAL PROCESS
---------------
-Get sensors, doors
-    Check safety, handle issues
-
-Check user input    
-    Handle any input
-
-Update:
-    Lights
-    UI
-
-    
-OPEN DOOR
----------
-Get sensors, doors
-    Check safety
-
-Check user input
-
-Run door, valves, pressure
-
-Update:
-    Lights
-    UI
-
-EMERGENCY
---------
-Lights Full
-Update UI wth warning
-
-Close doors
-
-Run valve, pressure
-    Safety checks always
-    
-"""
